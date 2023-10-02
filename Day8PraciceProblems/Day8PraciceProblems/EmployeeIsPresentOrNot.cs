2690e8f437e9ca657bca076b889f6754f25afa95
udaykumar721 <uday56367@gmail.com.com>
2023-10-02 22:31:01 +0530

    [UDAY]add : Check Employee Is Present Or Not

---
+++ Day8PraciceProblems/Day8PraciceProblems/EmployeeIsPresentOrNot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8PraciceProblems
+{
+    internal class EmployeeIsPresentOrNot
+    {
+        public void EmployeePresentOrAbsent()
+        {
+            //constants
+            int Is_Full_Time = 1;
+            Random rnd = new Random();
+            //Computation
+            int empCheck = rnd.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Not Present");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day8PraciceProblems/Day8PraciceProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8PraciceProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeIsPresentOrNot epom = new EmployeeIsPresentOrNot();
+            epom.EmployeePresentOrAbsent();
+            Console.ReadLine();
+        }
+    }
+}
